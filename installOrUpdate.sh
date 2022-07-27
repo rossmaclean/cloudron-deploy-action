075552f72a45b1a92f3ef8f1f46b2699ce0f0b8c
Ross MacLean <ross@rossmac.co.uk>
2022-07-27 22:14:54 +0100

    Add debugging.

--- installOrUpdate.sh
+++ installOrUpdate.sh
@@ -6,17 +6,30 @@ CLOUDRON_SERVER=$3
 CLOUDRON_TOKEN=$4
 INSTALL_IF_MISSING=$5
 
-APP_EXISTS=$(cloudron list --server $CLOUDRON_SERVER --token $CLOUDRON_TOKEN | grep $APP_DOMAIN | wc -l)
-echo "APP_ALREADY_INSTALLED=$APP_EXISTS" >> $GITHUB_ENV
+function installOrUpdate() {
+  echo "cloudron list"
+  echo $(cloudron list --server $CLOUDRON_SERVER --token $CLOUDRON_TOKEN)
 
-if [ "$APP_EXISTS" = "0" ]; then
-  if [ "$INSTALL_IF_MISSING" = "true" ]; then
-    echo "App does not exist, installing"
+  echo "cloudron list with grep"
+  echo $(cloudron list --server $CLOUDRON_SERVER --token $CLOUDRON_TOKEN | grep $APP_DOMAIN)
+
+  echo "cloudron list with grep and wc"
+  echo $(cloudron list --server $CLOUDRON_SERVER --token $CLOUDRON_TOKEN | grep $APP_DOMAIN | wc -l)
+
+  APP_EXISTS=$(cloudron list --server $CLOUDRON_SERVER --token $CLOUDRON_TOKEN | grep $APP_DOMAIN | wc -l)
+  echo "APP_ALREADY_INSTALLED=$APP_EXISTS" >>$GITHUB_ENV
+
+  if [ "$APP_EXISTS" = "0" ]; then
+    if [ "$INSTALL_IF_MISSING" = "true" ]; then
+      echo "App does not exist, installing"
       cloudron install --server $CLOUDRON_SERVER --token $CLOUDRON_TOKEN --location $APP_DOMAIN --image $DOCKER_IMAGE
+    else
+      echo "App does not exist, and install-if-missing is false, so doing nothing"
+    fi
   else
-    echo "App does not exist, and install-if-missing is false, so doing nothing"
+    echo "App exists, updating"
+    cloudron update --no-backup --server $CLOUDRON_SERVER --token $CLOUDRON_TOKEN --app $APP_DOMAIN --image $DOCKER_IMAGE
   fi
-else
-  echo "App exists, updating"
-  cloudron update --no-backup --server $CLOUDRON_SERVER --token $CLOUDRON_TOKEN --app $APP_DOMAIN --image $DOCKER_IMAGE
-fi
+}
+
+installOrUpdate
