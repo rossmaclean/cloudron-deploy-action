e903e95f20e881dc10fc86f4dee8df9352a9e805
Ross MacLean <ross@rossmac.co.uk>
2022-07-30 17:08:38 +0100

    Add env setting.

--- README.md
+++ README.md
@@ -40,11 +40,8 @@ Should the app be installed if not currently? False results in using update only
 
 When true, the app will not be backup up when updating
 
-[//]: # (### Environment)
+### Environment
+- **Key**: environment
+- **Required**: false
 
-[//]: # (- **Key**: environment)
-
-[//]: # (- **Required**: false)
-
-[//]: # ()
-[//]: # (Environment variables to be set. Note that this does not clear currently set environment variables which are not overwritten.)
+Environment variables to be set in format `KEY1=Val1 KEY2=Val2`. Note that this does not clear currently set environment variables which are not overwritten.
--- installOrUpdate.sh
+++ installOrUpdate.sh
@@ -11,37 +11,30 @@ ENV_VARS=$7
 function setEnvVars() {
   if [ -n "$ENV_VARS" ]; then
     echo "Setting environment variables"
-    IFS=';'
-    read -ra vars <<< "$ENV_VARS"
-    for var in "${vars[@]}";
-    do
-     IFS='='
-     read -ra keyval <<< "$var"
-     echo "Setting var $keyval[0] to $keyval[1]"
-    done
+    cloudron env set $ENV_VARS --server $CLOUDRON_SERVER --token $CLOUDRON_TOKEN --app $APP_DOMAIN
   fi
 }
 
 function installOrUpdate() {
-#  APP_EXISTS=$(cloudron list --server $CLOUDRON_SERVER --token $CLOUDRON_TOKEN | grep $APP_DOMAIN | wc -l)
-#  echo "APP_ALREADY_INSTALLED=$APP_EXISTS" >>$GITHUB_ENV
-#
-#  if [ "$APP_EXISTS" = "0" ]; then
-#    if [ "$INSTALL_IF_MISSING" = "true" ]; then
-#      echo "App does not exist, installing"
-#      cloudron install --server $CLOUDRON_SERVER --token $CLOUDRON_TOKEN --location $APP_DOMAIN --image $DOCKER_IMAGE
-#    else
-#      echo "App does not exist, and install-if-missing is false, so doing nothing"
-#    fi
-#  else
-#    echo "App exists, updating"
-#    if [ "$SKIP_BACKUP" = "true" ]; then
-#      echo "SKIP_BACKUP is true, skipping backup"
-#      cloudron update --no-backup --server $CLOUDRON_SERVER --token $CLOUDRON_TOKEN --app $APP_DOMAIN --image $DOCKER_IMAGE
-#    else
-#      cloudron update --server $CLOUDRON_SERVER --token $CLOUDRON_TOKEN --app $APP_DOMAIN --image $DOCKER_IMAGE
-#    fi
-#  fi
+  APP_EXISTS=$(cloudron list --server $CLOUDRON_SERVER --token $CLOUDRON_TOKEN | grep $APP_DOMAIN | wc -l)
+  echo "APP_ALREADY_INSTALLED=$APP_EXISTS" >>$GITHUB_ENV
+
+  if [ "$APP_EXISTS" = "0" ]; then
+    if [ "$INSTALL_IF_MISSING" = "true" ]; then
+      echo "App does not exist, installing"
+      cloudron install --server $CLOUDRON_SERVER --token $CLOUDRON_TOKEN --location $APP_DOMAIN --image $DOCKER_IMAGE
+    else
+      echo "App does not exist, and install-if-missing is false, so doing nothing"
+    fi
+  else
+    echo "App exists, updating"
+    if [ "$SKIP_BACKUP" = "true" ]; then
+      echo "SKIP_BACKUP is true, skipping backup"
+      cloudron update --no-backup --server $CLOUDRON_SERVER --token $CLOUDRON_TOKEN --app $APP_DOMAIN --image $DOCKER_IMAGE
+    else
+      cloudron update --server $CLOUDRON_SERVER --token $CLOUDRON_TOKEN --app $APP_DOMAIN --image $DOCKER_IMAGE
+    fi
+  fi
 
   setEnvVars
 }
